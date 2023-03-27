2054f26348f9adfa3f3c8c46e2ae9805c0f16253
Petar Stanev <petar.stanev1987@gmail.com>
2023-03-27 18:33:25 +0300

    Added authentication on client side
    Added login and register modal components

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class ActivitiesController : BaseController
     {
         [HttpGet] // api/activities
